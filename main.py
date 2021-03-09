35d1aeda8a25e73371981f67ef2ce5a69402f407
Josrodjr <Josrodjr@gmail.com>
2021-03-09 02:06:20 -0600

    generate the CSV

--- main.py
+++ main.py
@@ -2,9 +2,13 @@
 
 from steamapi import core, user
 import pandas as pd
+import time
 
 core.APIConnection(api_key="", validate_key=True)
 steam_user = 0
+# generate a dataset to dump the data
+column_names = ["name", "real_name", "country_code", "level", "xp", "bans", "friends", "games", "recently_played_games", "owned_games", "badges"]
+dataframe_steam = pd.DataFrame(columns = column_names)
 
 try:
     # query the username to the api
@@ -25,9 +29,14 @@ try:
     owned_games = steam_user.owned_games
     badges = steam_user.badges
 
-    # print(name, real_name, country_code, level, xp, bans, friends, games, recently_played_games, owned_games, badges)
+    # insert the data into the dataframe
+    dataframe_steam = dataframe_steam.append(pd.Series([name, real_name, country_code, level, xp, bans, friends, games, recently_played_games, owned_games, badges]), ignore_index=True)
+    # print(name, real_name, country_code, level, xp, bans, friends, games, recently_played_games, owned_games, badges)    
     
 except Exception as error:
     print("Error: " + error)
+
+# get the dataframe into a CSV
+dataframe_steam.to_csv('steam_profiles.csv', index=None)
 
 
